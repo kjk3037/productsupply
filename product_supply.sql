5cfd3f755a38428f39b280586be9ff8b7691ea4b
kjk3037 <87018057@qq.com>
2021-08-17 19:00:53 +0800

    new sql from 8/17

--- product_supply.sql
+++ product_supply.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50734
+Source Server Version : 50731
 Source Host           : localhost:3306
 Source Database       : product_supply
 
 Target Server Type    : MYSQL
-Target Server Version : 50734
+Target Server Version : 50731
 File Encoding         : 65001
 
-Date: 2021-07-27 12:33:43
+Date: 2021-08-17 19:00:21
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,7 +23,7 @@ CREATE TABLE `account_authority` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `describe` char(20) NOT NULL COMMENT '权限描述',
   `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否启用（0禁用，1启用）',
-  `module` varchar(255) NOT NULL,
+  `module_id` varchar(255) NOT NULL,
   `role_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -38,7 +38,7 @@ CREATE TABLE `account_authority` (
 DROP TABLE IF EXISTS `account_dept`;
 CREATE TABLE `account_dept` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL COMMENT '部门名称',
+  `dept_name` varchar(255) DEFAULT NULL COMMENT '部门名称',
   `desc` varchar(255) DEFAULT NULL,
   `parent_id` int(11) DEFAULT NULL COMMENT '上级部门',
   PRIMARY KEY (`id`)
@@ -121,15 +121,16 @@ CREATE TABLE `account_user` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `nickname` varchar(255) DEFAULT NULL,
-  `role_id` char(32) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL COMMENT '1、使用中；2、注册中；3、冻结中；0、注销',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of account_user
 -- ----------------------------
-INSERT INTO `account_user` VALUES ('1', 'root', 'admin', '超级管理', '');
-INSERT INTO `account_user` VALUES ('2', 'kjk', '1122', '本人', null);
+INSERT INTO `account_user` VALUES ('1', 'root', 'admin', '超级管理', '1');
+INSERT INTO `account_user` VALUES ('2', 'kjk', '1122', '本人', '1');
+INSERT INTO `account_user` VALUES ('2ed6aca54cc9476297bd27de92ade417', 'jack', '123', 'jack123', '1');
 
 -- ----------------------------
 -- Table structure for account_user_role_relation
@@ -153,7 +154,7 @@ DROP TABLE IF EXISTS `dict`;
 CREATE TABLE `dict` (
   `id` int(11) NOT NULL,
   `type_id` int(11) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `dict_name` varchar(255) DEFAULT NULL,
   `desc` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -3610,7 +3611,7 @@ DROP TABLE IF EXISTS `material_type`;
 CREATE TABLE `material_type` (
   `atid` int(5) NOT NULL AUTO_INCREMENT COMMENT '自增计数',
   `number` varchar(255) NOT NULL COMMENT '物料代码',
-  `name` varchar(255) DEFAULT NULL COMMENT '物料名称',
+  `material_name` varchar(255) DEFAULT NULL COMMENT '物料名称',
   `standard` varchar(255) DEFAULT NULL COMMENT '规格',
   `technology` varchar(255) DEFAULT NULL COMMENT '工艺',
   `brand` varchar(255) DEFAULT NULL COMMENT '品牌',
@@ -5250,9 +5251,9 @@ CREATE TABLE `purchase_order` (
   `warehousing_status` varchar(255) DEFAULT NULL COMMENT '入库核销完成',
   `is_invoice` varchar(255) DEFAULT NULL COMMENT '有无发票',
   `invoice_type` varchar(255) DEFAULT NULL COMMENT '发票类型',
-  `jd_purchase_other_number` char(11) DEFAULT NULL COMMENT '金蝶采购订单编码',
+  `jd_purchase_ordernumber` char(11) DEFAULT NULL COMMENT '金蝶采购订单编码',
   `payment_method` int(255) DEFAULT NULL COMMENT '收付方式',
-  `other_paymen_method_reason` varchar(255) DEFAULT NULL COMMENT '其他收付方式',
+  `other_paymentmethod_reason` varchar(255) DEFAULT NULL COMMENT '其他收付方式',
   `full_payment_status` varchar(255) DEFAULT NULL COMMENT '订单支付完成',
   `warehousing` varchar(255) DEFAULT NULL COMMENT '入库单附件',
   `invoice` varchar(255) DEFAULT NULL COMMENT '发票附件',
@@ -5399,15 +5400,16 @@ INSERT INTO `sys_sub_module` VALUES ('10', '来料检验', '/imcomingCheck', '5'
 DROP TABLE IF EXISTS `workflew`;
 CREATE TABLE `workflew` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `wf_name` varchar(255) DEFAULT NULL,
   `desc` varchar(255) DEFAULT NULL,
+  `sys_module_id` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of workflew
 -- ----------------------------
-INSERT INTO `workflew` VALUES ('1', '采购支付申请审批流程', null);
+INSERT INTO `workflew` VALUES ('1', '采购支付申请审批流程', null, null);
 
 -- ----------------------------
 -- Table structure for workflew_approver
@@ -5432,12 +5434,13 @@ CREATE TABLE `workflew_approver` (
 DROP TABLE IF EXISTS `workflew_entity`;
 CREATE TABLE `workflew_entity` (
   `id` int(11) NOT NULL,
+  `module_entity_id` int(11) DEFAULT NULL,
   `workflew_id` int(11) DEFAULT NULL,
   `workflew_node_id` int(11) DEFAULT NULL,
-  `entity_id` char(32) DEFAULT NULL COMMENT '表单实例id（例子：purchase_order:id）',
   `approver_id` int(11) DEFAULT NULL,
   `approver_result` varchar(255) DEFAULT NULL,
   `approver_suggestion` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL COMMENT '1、运行中；2、已完成；3、已终止',
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -5446,6 +5449,7 @@ CREATE TABLE `workflew_entity` (
 -- ----------------------------
 -- Records of workflew_entity
 -- ----------------------------
+INSERT INTO `workflew_entity` VALUES ('1', '5', '1', '1', '1', null, null, '1', null, null);
 
 -- ----------------------------
 -- Table structure for workflew_node
@@ -5453,17 +5457,57 @@ CREATE TABLE `workflew_entity` (
 DROP TABLE IF EXISTS `workflew_node`;
 CREATE TABLE `workflew_node` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `workflew_id` int(11) DEFAULT NULL,
-  `approver_id` int(11) DEFAULT NULL,
+  `node_type` int(11) NOT NULL COMMENT '0、开始节点；1、审批节点；2、填写节点；3、分支节点；4、抄送节点；5、结束节点',
   `node_name` varchar(255) DEFAULT NULL,
-  `prev_node_id` int(11) DEFAULT NULL,
-  `next_node_id` int(11) DEFAULT NULL,
+  `workflew_id` int(11) DEFAULT NULL COMMENT '流程id',
+  `prev_node_id` int(11) DEFAULT NULL COMMENT '前节点',
+  `next_node_id` int(11) DEFAULT NULL COMMENT '后节点',
+  `create_date` datetime DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of workflew_node
 -- ----------------------------
-INSERT INTO `workflew_node` VALUES ('1', '1', '1', '交付经理审批', null, '2');
-INSERT INTO `workflew_node` VALUES ('2', '1', '2', '运营会计审批', '1', '3');
-INSERT INTO `workflew_node` VALUES ('3', '1', '3', '总账会计审批', '2', null);
+INSERT INTO `workflew_node` VALUES ('1', '1', '交付经理审批', '1', '4', '2', null, null);
+INSERT INTO `workflew_node` VALUES ('2', '1', '运营会计审批', '1', '1', '6', null, null);
+INSERT INTO `workflew_node` VALUES ('3', '1', '总账会计审批', '1', '2', '5', null, null);
+INSERT INTO `workflew_node` VALUES ('4', '0', '提交节点', '1', null, '1', null, null);
+INSERT INTO `workflew_node` VALUES ('5', '5', '结束节点', '1', '3', null, null, null);
+INSERT INTO `workflew_node` VALUES ('6', '3', '分支节点', '1', '2', null, null, null);
+
+-- ----------------------------
+-- Table structure for workflow_branch
+-- ----------------------------
+DROP TABLE IF EXISTS `workflow_branch`;
+CREATE TABLE `workflow_branch` (
+  `id` int(11) NOT NULL,
+  `branch_name` varchar(255) DEFAULT NULL,
+  `workflow_id` int(11) DEFAULT NULL,
+  `workflow_node_id` int(11) DEFAULT NULL,
+  `condition` varchar(255) DEFAULT NULL,
+  `workflow_nextnode_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of workflow_branch
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for workflow_table_field
+-- ----------------------------
+DROP TABLE IF EXISTS `workflow_table_field`;
+CREATE TABLE `workflow_table_field` (
+  `id` int(11) NOT NULL,
+  `workflow_id` int(11) DEFAULT NULL,
+  `workflow_node_id` int(11) DEFAULT NULL,
+  `field_id` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL COMMENT '1、可见；2、可编辑；3、隐藏；4、禁用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of workflow_table_field
+-- ----------------------------
